2ed9575357aee148e0bc643399a1f4d7b68fb44e
Jorge Paris Junior <mrjorgeparis@gmail.com>
2025-01-05 00:18:57 +0100

    Ainda tentando criar layout responsive em mobile

--- app/src/Productos.tsx
+++ app/src/Productos.tsx
@@ -3,18 +3,7 @@ import mainimage from "./static/fresh-veg-1.jpg";
 export function Produtos() {
   return (
     <>
-      <div className="container text-center">
-        <div className="row">
-          <div className="col">Column</div>
-          <div className="col">Column</div>
-          <div className="col">Column</div>
-        </div>
-        <div className="row">
-          <div className="col">Column</div>
-          <div className="col">Column</div>
-          <div className="col">Column</div>
-        </div>
-      </div>
+      
     </>
   );
 }
