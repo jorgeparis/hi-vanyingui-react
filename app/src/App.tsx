15877587c0794401f34f81178d0fec0b8216fc90
Jorge Paris Junior <mrjorgeparis@gmail.com>
2025-01-05 17:25:18 +0100

    SEM IDEAS

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,12 +8,18 @@ import { Produtos } from "./Productos";
 import { Button } from "./Button";
 
 function App() {
+  const alunos = [
+
+    {id:1,name:'Jorge Paris',isAlive:true},
+    {id:2,name:'Jorge Paris',isAlive:true},
+    {id:3,name:'Jorge Paris',isAlive:true},
+  ];
   return (
     <div className="container p-5 gap-10" style={{ background: "rede" }}>
       <Navbar />
       <Carousel />
       <Produtos />
-      <Button />
+      <Button alunos={alunos}/>
     </div>
   );
 }
--- app/src/Button.tsx
+++ app/src/Button.tsx
@@ -1,15 +1,12 @@
-export function Button(){
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
+export function Button({ alunos }) {
+  //PARA TRATAR EVENTOS APENAS CRIAMOS A FUNCAO/HANDLER DE EVENTOS E FAZEMOS SUA REFERENCIA NO ELEMENTO  A ESCUTAR O EVENTO
+  const lista_de_alunos = alunos.map((aluno) => {
+    <li key={aluno.id}>{aluno.name}</li>;
+  });
 
-    //PARA TRATAR EVENTOS APENAS CRIAMOS A FUNCAO/HANDLER DE EVENTOS E FAZEMOS SUA REFERENCIA NO ELEMENTO  A ESCUTAR O EVENTO
-
-    function handler(){
-
-        return alert('I was clicked')
-    }
-    return (
-        <>
-        <button type="button" className="btn btn-primary" onClick={handler}>Click Me</button>
-        </>
-    )
+  return <>
+  <ul>{lista_de_alunos}</ul>
+  </>;
 }
