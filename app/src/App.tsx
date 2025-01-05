60eff0a258047d10655dce31a0ff72f2db03704a
Jorge Paris Junior <mrjorgeparis@gmail.com>
2025-01-05 20:44:26 +0100

    mensagem

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,19 +7,12 @@ import { Carousel } from "./Carousel";
 
 function App() {
   return (
-    <div className="container p-5">
-      <div className="row mb-3">
+    <div className="container">
+      <div className="row">
         <Navbar />
       </div>
       <div className="row">
-        <Carousel />
-      </div>
-      <div className="row">
         <div className="col">
-          <h5>Categorias</h5>
-        </div>
-        <div className="col align-self-end">
-          <h3>Jorge</h3>
         </div>
       </div>
     </div>
