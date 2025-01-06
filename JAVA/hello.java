ea3fdd4bb28532ebe713fce38c537ca3f7582e89
Jorge Paris Junior <mrjorgeparis@gmail.com>
2025-01-06 09:25:57 +0100

    continuo aprendendo

---
+++ JAVA/hello.java
@@ -0,0 +1,15 @@
+
+package JAVA;
+
+public class hello {
+    static int soma(int a, int b){
+        return a+b;
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Ola Mundo");
+        System.out.println(soma(1,2));
+    }
+    
+
+}
